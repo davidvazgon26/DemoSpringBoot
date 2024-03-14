71273fcba729a4e5f02680bbff1d15481ee9e27c
davidvazgon26 <davidvazgon@hotmail.com>
2024-03-13 23:37:35 -0600

    primer commit

---
+++ src/main/java/com/demo/David/DemoDavidApplication.java
@@ -0,0 +1,13 @@
+package com.demo.David;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDavidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDavidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/David/DemoDavidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.David;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDavidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
