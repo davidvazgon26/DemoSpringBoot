10205e0372f5f61857d84ad81808eece6103d251
davidvazgon26 <davidvazgon@hotmail.com>
2024-03-20 23:55:59 -0600

    Usando Query Methods en Spring boot

--- davg/src/main/java/com/demo/davg/persistence/crud/ProductoCrudRepository.java
+++ davg/src/main/java/com/demo/davg/persistence/crud/ProductoCrudRepository.java
@@ -1,8 +1,12 @@
 package com.demo.davg.persistence.crud;
 
+// import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.demo.davg.persistence.entity.Producto;
+import java.util.List;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
-
+    // @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery
+    // = true);
+    List<Producto> findByIdCategoria(Integer idCategoria);
 }
