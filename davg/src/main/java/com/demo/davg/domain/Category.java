b5d1a66bcd3ad9a704457e333a2ec82260d21c67
davidvazgon26 <davidvazgon@hotmail.com>
2024-03-21 22:54:36 -0600

    creando mapa a BD desde la aplicacion con sus repositories para la estructura tipo dominio

---
+++ davg/src/main/java/com/demo/davg/domain/Category.java
@@ -0,0 +1,31 @@
+package com.demo.davg.domain;
+
+public class Category { // Mapeando nuestros datos en la aplicacion con los de la BD
+    private int categoryId;
+    private String category;
+    private boolean active;
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
--- davg/src/main/java/com/demo/davg/persistence/ProductoRepository.java
+++ davg/src/main/java/com/demo/davg/persistence/ProductoRepository.java
@@ -1,11 +1,15 @@
 package com.demo.davg.persistence;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
 
 import com.demo.davg.persistence.crud.ProductoCrudRepository;
 import com.demo.davg.persistence.entity.Producto;
 
-public class ProductoRepository {
+@Repository // Esta notacion le indica a Spring boot que esta clase interactua con la BD
+public class ProductoRepository { // Repository es la que interactua directamente con la BD.
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll() {
@@ -14,5 +18,18 @@ public class ProductoRepository {
 
     public List<Producto> getByProductos(int idCategoria) {
         return productoCrudRepository.findByIdCategoria(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessTHanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    @SuppressWarnings("null")
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
     }
 }
